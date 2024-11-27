8b0d71a0a2c628c770409089d22d2303c88bc8f2
ssmahim01 <mahimzayn666@gmail.com>
2024-11-28 01:44:31 +0600

    Updated Footer then FollowUs component and Rename two image of assets folder

--- src/components/FollowUs.jsx
+++ src/components/FollowUs.jsx
@@ -1,14 +1,16 @@
+import { FaInstagram } from "react-icons/fa";
 import CoffeesImgContainer from "./coffeesImgContainer";
+import { Link } from "react-router-dom";
 
 const FollowUs = () => {
   return (
     <div>
-        <h5 className="md:text-xl text-[#1B1A1A] text-center pb-2 font-serif">Follow Us Now</h5>
+        <Link to="https://www.instagram.com/iammz01/" className="md:text-xl text-[#1B1A1A] pb-2 font-serif flex justify-center items-center gap-2" target="_blank"><h5>Follow Us Now</h5> <FaInstagram className="text-xl text-rose-500"/></Link>
       <h3 className="md:text-5xl text-4xl text-[#331A15] text-center font-bold font-rancho">
         Follow On Instagram
       </h3>
 
-      <section className="md:w-4/5 w-11/12 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-7 py-14">
+      <section className="md:w-4/5 w-11/12 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-7 py-14 mb-10">
        <CoffeesImgContainer></CoffeesImgContainer>
       </section>
     </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,42 +1,109 @@
+import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
+import coffeeImg from "/src/assets/more/logo1.png";
+import { MdEmail } from "react-icons/md";
+import { FaLocationDot } from "react-icons/fa6";
+import { IoMdCall } from "react-icons/io";
+
 const Footer = () => {
   return (
-    <footer className="footer bg-base-200 text-base-content p-10">
-      <nav>
-        <h6 className="footer-title">Services</h6>
-        <a className="link link-hover">Branding</a>
-        <a className="link link-hover">Design</a>
-        <a className="link link-hover">Marketing</a>
-        <a className="link link-hover">Advertisement</a>
-      </nav>
-      <nav>
-        <h6 className="footer-title">Company</h6>
-        <a className="link link-hover">About us</a>
-        <a className="link link-hover">Contact</a>
-        <a className="link link-hover">Jobs</a>
-        <a className="link link-hover">Press kit</a>
-      </nav>
-      <nav>
-        <h6 className="footer-title">Legal</h6>
-        <a className="link link-hover">Terms of use</a>
-        <a className="link link-hover">Privacy policy</a>
-        <a className="link link-hover">Cookie policy</a>
-      </nav>
-      <form>
-        <h6 className="footer-title">Newsletter</h6>
-        <fieldset className="form-control w-80">
-          <label className="label">
-            <span className="label-text">Enter your email address</span>
-          </label>
-          <div className="join">
-            <input
-              type="text"
-              placeholder="username@site.com"
-              className="input input-bordered join-item"
-            />
-            <button className="btn btn-primary join-item">Subscribe</button>
+    <footer
+      style={{
+        backgroundImage: "url('/src/assets/more/footer-bg.jpg')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <div className="md:w-4/5 w-11/12 mx-auto flex justify-between items-center p-14 gap-5">
+        <div className="md:w-1/2 pt-12">
+          <div className="flex flex-col gap-5">
+            <figure>
+              <img
+                className="w-16 h-16"
+                src={coffeeImg}
+                alt="Image of coffee logo"
+              />
+            </figure>
+
+            <h2 className="md:text-4xl text-3xl text-[#331A15] font-bold font-rancho">
+              Espresso Emporium
+            </h2>
+
+            <p className="md:text-base text-sm">
+              Always ready to be your friend. Come & Contact with us to share
+              your memorable moments, to share with your best companion.
+            </p>
+
+            <div className="flex gap-4 items-center *:text-[#331A15] *:w-10 *:h-10 *:cursor-pointer">
+              <FaFacebook />
+              <FaTwitter />
+              <FaInstagram />
+              <FaLinkedin />
+            </div>
+
+            <h2 className="md:text-4xl text-3xl text-[#331A15] font-bold font-rancho">
+              Get in Touch
+            </h2>
+
+            <div className="flex gap-3 items-center *:text-lg font-serif font-medium">
+              <IoMdCall /> <span>+88 01533 333 333</span>
+            </div>
+
+            <div className="flex gap-3 items-center *:text-lg font-serif font-medium">
+              <MdEmail /> <span>espresso@gmail.com</span>
+            </div>
+
+            <div className="flex gap-3 items-center *:text-lg font-serif font-medium">
+              <FaLocationDot /> <span>72, Wall street, King Road, Dhaka</span>
+            </div>
           </div>
-        </fieldset>
-      </form>
+        </div>
+        <div className="md:w-2/5 pt-14">
+          <h2 className="md:text-4xl text-3xl text-[#331A15] font-bold font-rancho pb-10">
+            Connect with Us
+          </h2>
+
+          <form className="space-y-5">
+            <div className="form-control">
+              <input
+                type="text"
+                placeholder="Name"
+                className="input input-bordered w-full pl-4"
+                required
+              />
+            </div>
+
+            <div className="form-control">
+              <input
+                type="email"
+                placeholder="Email"
+                className="input input-bordered w-full pl-4"
+                required
+              />
+            </div>
+
+            <div className="form-control">
+              <input
+                placeholder="Message"
+                className="input input-bordered w-full h-32 pb-20 pl-4"
+              ></input>
+            </div>
+
+            <button className="form-control btn btn-outline border-2 border-[#331A15] text-[#331A15] text-xl font-rancho font-semibold px-4 rounded-full">
+              Send Message
+            </button>
+          </form>
+        </div>
+      </div>
+
+      <div className="h-12" style={{
+        backgroundImage: "url('/src/assets/more/coffee-seeds.jpg')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat"
+      }}>
+        <p className="text-xl text-white font-medium font-rancho text-center pt-2">Copyright Espresso Emporium ! All Rights Reserved</p>
+      </div>
     </footer>
   );
 };
